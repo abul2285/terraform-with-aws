25e1737f0d87596117c9e43937e143d4ebda57d9
Abul Hossain <mdabulhossain780@gmail.com>
2023-04-27 23:14:02 +0600

    feat: a task defination code has been added

--- main.tf
+++ main.tf
@@ -9,11 +9,35 @@ terraform {
 
 provider "aws" {}
 
-resource "aws_ecr_repository" "crr-via-terraform" {
-  name                 = "hello-terraform"
+resource "aws_ecr_repository" "ecr-1" {
+  name                 = "hello-ecr"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+}
+
+resource "aws_ecs_task_definition" "task-1" {
+  family                   = "hello-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "iis",
+    "image": "mcr.microsoft.com/windows/servercore/iis",
+    "cpu": 1024,
+    "memory": 2048,
+    "essential": true
+  }
+]
+TASK_DEFINITION
+
+  runtime_platform {
+    operating_system_family = "WINDOWS_SERVER_2019_CORE"
+    cpu_architecture        = "X86_64"
   }
 }
