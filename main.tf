75fb1c81727b090202d6dc332d37fcefb646228a
Abul Hossain <mdabulhossain780@gmail.com>
2023-04-27 23:16:25 +0600

    feat: security group has been added

--- main.tf
+++ main.tf
@@ -58,3 +58,28 @@ resource "aws_vpc" "vpc-1" {
     Name = "hello-vpc"
   }
 }
+
+resource "aws_security_group" "sg-1" {
+  name        = "hello-sg"
+  vpc_id      = aws_vpc.vpc-1.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc-1.cidr_block]
+    # ipv6_cidr_blocks = [aws_vpc.vpc-1.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "hello-sg"
+  }
+}
